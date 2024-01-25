1d1987054e334e6c6471a6b8a375e7226674d0c5
CursedDomino <dillon.daughrity@outlook.com>
2024-01-25 12:07:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ def sum_of_products(list1, list2):
         sum += list1[list1.index(num)] * list2[list1.index(num)]
     return sum
 
-list1 = [1, 2, 3]
-list2 = [4, 5, 6]
-
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
     sum = sum_of_products(list1, list2)
