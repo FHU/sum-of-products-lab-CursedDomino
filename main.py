fd4dcab19a12728f131816050bb6977379810601
CursedDomino <dillon.daughrity@outlook.com>
2024-02-04 13:28:08 -0600

    Test

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 def sum_of_products(list1, list2):
     sum = 0
-    if len(num1) == len(num2):
-        for i in num1:
-            sum += int(num1[num1.index(i)]) * int(num2[num1.index(i)])
+    count = 0
+    if len(list1) == len(list2):
+        for i in list1:
+            sum += int(list1[count]) * int(list2[count])
+            count += 1
         return sum
 
-num1 = input()
-num2 = input()
+list1 = input()
+list2 = input()
+
+list1 = list1.split()
+list2 = list2.split()
 
 if __name__ == '__main__':
-    sum = sum_of_products(num1, num2)
+    sum = sum_of_products(list1, list2)
     print(sum)
